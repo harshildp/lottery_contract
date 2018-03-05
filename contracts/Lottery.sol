f0c20bbb421624303e348913367f8d37cf9002b9
Harshil Patel <h.gibbs62@gmail.com>
2018-03-04 18:14:27 -0600

    restricted modifier and contract reset

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -10,6 +10,7 @@ contract Lottery {
 
     function enter() public payable {
         require(msg.value >= .01 ether);
+
         players.push(msg.sender);
     }
 
@@ -17,8 +18,18 @@ contract Lottery {
         return uint(keccak256(block.difficulty, now, players));
     }
 
-    function pickWinner() public {
+    function pickWinner() public restricted {
         uint index = random() % players.length;
         players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
     }
 }   
